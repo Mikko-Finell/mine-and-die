c0fc29739a935aafb5e03ff6c659b8c772bced61
Mikko Finell <mikko.finell@gmail.com>
2025-10-16 14:14:21 +0300

    chore: scaffold effectsgen workspace

---
+++ tools/effectsgen/internal/pipeline/doc.go
@@ -0,0 +1,5 @@
+// Package pipeline will contain the discrete stages of the effects contract
+// generation workflow (loading contracts, validating registries, emitting
+// client bindings). Implementation will follow the roadmap once the scaffolding
+// is in place.
+package pipeline
