e86fdb5bd54e9516fa001273af1192f4ae93f45e
Mikko Finell <mikko.finell@gmail.com>
2025-10-17 08:40:12 +0300

    Add go:generate wiring for effectsgen pipeline

---
+++ server/effects/contract/generate.go
@@ -0,0 +1,3 @@
+//go:generate go run mine-and-die/tools/effectsgen/cmd/effectsgen --contracts=. --registry=definitions.go --definitions=../../../config/effects/definitions.json --out=../../../client/generated/effect-contracts.ts
+
+package contract
